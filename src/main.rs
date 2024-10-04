23149d38493768373513d029af184078b428b9f7
Jaime Blandon <blandon.jaime@gmail.com>
2024-10-04 17:30:59 -0600

    intro migraciones

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conexion = bd_con_rust::obtener_conexion().await?;
     println!("conectado!");
 
-    let crear_tabla = Statement::from_string(
-        conexion.get_database_backend(),
-        r#"
-        CREATE TABLE prueba(
-            id int,
-            nombre varchar(150)
-        )
-        "#,
-    );
-
-    conexion.execute(crear_tabla).await?;
-
-    let insert = Statement::from_sql_and_values(
-        conexion.get_database_backend(),
-        r#"
-        INSERT INTO prueba(id, nombre)
-        VALUES($1, $2)
-        "#,
-        [1.into(), "Rusty Full Stack".into()],
-    );
-
-    conexion.execute(insert).await?;
-
-    println!("Registro exitoso!");
     Ok(())
 }
