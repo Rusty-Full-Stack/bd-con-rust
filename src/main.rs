2c5f94612343aa9e68dd728aa3f436783a22b070
Jaime Blandon <blandon.jaime@gmail.com>
2024-10-15 23:25:01 -0600

    actualizacion

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,16 @@ use sea_orm::*;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conexion = bd_con_rust::obtener_conexion().await?;
 
-    let nuevo_producto = producto::ActiveModel {
-        nombre: ActiveValue::Set("Nuevo producto".to_owned()),
-        precio_unitario: ActiveValue::Set(Decimal::from_f64_retain(100.50).unwrap()),
+    let producto_actualizar = producto::ActiveModel {
+        id: ActiveValue::Set(1),
+        nombre: ActiveValue::Set("Cafetera".to_owned()),
+        descripcion: ActiveValue::Set(Some("Mi maravillosa cafetera!".to_owned())),
         ..Default::default()
     };
 
-    let resultado_insert_producto = Producto::insert(nuevo_producto).exec(&conexion).await?;
+    producto_actualizar.update(&conexion).await?;
 
-    let nueva_factura = factura::ActiveModel {
-        nombre_cliente: ActiveValue::Set(Some("Rusty Full Stack".to_owned())),
-        total: ActiveValue::Set(Some(Decimal::from_f64_retain(100.50).unwrap())),
-        ..Default::default()
-    };
-
-    let resultado_insert_factura = Factura::insert(nueva_factura).exec(&conexion).await?;
-
-    let detalle = detalle_factura::ActiveModel {
-        producto_id: ActiveValue::Set(resultado_insert_producto.last_insert_id),
-        factura_id: ActiveValue::Set(resultado_insert_factura.last_insert_id),
-        cantidad: ActiveValue::Set(1),
-        subtotal: ActiveValue::Set(Some(Decimal::from_f64_retain(100.50).unwrap())),
-    };
-
-    DetalleFactura::insert_many(vec![detalle])
-        .exec(&conexion)
-        .await?;
-
-    println!("Registros exitosos");
+    println!("Actualizacion exitosa!");
 
     Ok(())
 }
