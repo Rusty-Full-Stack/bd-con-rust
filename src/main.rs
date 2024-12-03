18f3b979c92872384c3ec1f0b601b003b40d145e
Jaime Blandon <blandon.jaime@gmail.com>
2024-12-02 20:47:09 -0600

    filtros

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,39 @@ use entities::prelude::*;
 use entities::*;
 
 use sea_orm::prelude::*;
-use sea_orm::*;
+use sea_orm::Condition;
+use sea_orm::QueryTrait;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let conexion = bd_con_rust::obtener_conexion().await?;
 
-    let resultado: Option<producto::Model> = Producto::find_by_id(2).one(&conexion).await?;
+    let resultado: Vec<producto::Model> = Producto::find()
+        .filter(
+            Condition::any()
+                .add(producto::Column::PrecioUnitario.gte(100.0))
+                .add(producto::Column::Nombre.starts_with("Cafe")),
+        )
+        .all(&conexion)
+        .await?;
 
-    if let Some(producto_encontrado) = resultado {
-        println!("producto encontrado es: ");
-        println!("id: {}", producto_encontrado.id);
-        println!("nombre: {}", producto_encontrado.nombre);
-    } else {
-        println!("producto no encontrado");
-    };
+    for mi_producto in resultado {
+        println!("Id: {}", mi_producto.id);
+        println!("Nombre: {}", mi_producto.nombre);
+        println!("Precio Unitario: {}", mi_producto.precio_unitario);
+        println!("=======================================");
+    }
+
+    let query = Producto::find()
+        .filter(
+            Condition::any()
+                .add(producto::Column::PrecioUnitario.gte(100.0))
+                .add(producto::Column::Nombre.starts_with("Cafe")),
+        )
+        .build(sea_orm::DatabaseBackend::Postgres)
+        .to_string();
+
+    println!("El query es: {}", query);
 
     Ok(())
 }
